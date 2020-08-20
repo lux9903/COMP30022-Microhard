044563dbe2e067b004afb6a04aac81738608dbb7
WujiZhu4554 <wujiz1@unimelb.edu.au>
2020-08-21 00:35:19 +1000

    a

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -18,6 +19,9 @@ const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log("MongoDB database connection established successfully");
 })
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
