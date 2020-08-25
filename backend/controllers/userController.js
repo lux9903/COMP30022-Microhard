ea63ad39ac69f189646c1b166839008a3b76887a
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-08-25 22:04:04 +0700

    update authentication backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,6 @@ const login = async (req,res) => {
     // Create and assign a token
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
     res.header('auth-token',token).send(token);
-    res.send("Log in")
 }
 module.exports = {
     getUser,
