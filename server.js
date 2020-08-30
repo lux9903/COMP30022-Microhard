a79834ba391de9337714fb20075049fe6d91db91
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-08-30 23:49:05 +0800

    Fix deployment bug

--- server.js
+++ server.js
@@ -1,37 +1,123 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const session = require('express-session');
 const path = require('path');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const mongoose = require('mongoose');
+const compression = require('compression');
+const MongoStore = require('connect-mongo')(session);
 
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(cors());
-app.use(bodyParser.json());
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
+// connect to DB
+mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: true,
   useUnifiedTopology: true,
 });
 
-const {connection} = mongoose;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+// Tell Mongoose to use ES6 promises
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+  console.error(err.message);
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('autoIndex', false);
 
-const exercisesRouter = require('./backend/routes/exercises');
-const usersRouter = require('./backend/routes/users');
+// Create global app object
+const app = express();
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+app.use(cors());
+app.use(compression());
 
-app.listen(port, () => {
-  console.log(`Server is running on port : ${port}`);
+// Normal express config defaults
+app.use(require('morgan')('dev'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+
+app.use(
+  session({
+    secret: 'web-it',
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Models
+
+require('./server/models/userModel');
+
+// Authentication
+require('./server/config/passport');
+
+// Routes
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(require('./server/routes'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });
+
+/// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(errorhandler());
+
+  mongoose.set('debug', true);
+
+  app.use(function (err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+// finally, let's start our server...
+const server = app.listen(process.env.PORT || 5000, function () {
+  console.log(
+    `Listening on port ${server.address().port}. Open http://localhost:${
+      server.address().port
+    }`
+  );
+});
+
+module.exports = server;
