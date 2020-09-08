adafc2536013b7fc478128dd3e0a5542283f9f95
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-09-08 13:33:22 +0800

    Fix connection between backend and frontend
    - Removed redundant simple backend folder and repeated home.jsx file

--- server.js
+++ server.js
@@ -70,9 +70,9 @@ require('./server/config/passport');
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(require('./server/routes'));
 
-//app.get('*', (req, res) => {
-//  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
-//});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/client/public/index.html`));
+});
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
