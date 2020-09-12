d4dd9478fce8a60d129c2c246355fea28de409d8
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-09-13 02:49:19 +0800

    Change b/w to coloured favicon and add animation

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import Grow from '@material-ui/core/Grow';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,75 +33,79 @@ export default function Hero() {
     <div className={classes.root}>
       <Container fixed>
         <Grid container justify="center" direction="row" alignItems="center">
-          <Grid item xs={12} md={6}>
-            <Grid
-              container
-              justify="center"
-              direction="column"
-              alignItems="center"
-            >
-              <Typography>
-                <br />
-                <br />
-                <br />
-              </Typography>
-              <Grid item xs={8}>
-                <Typography variant="h3" align="center">
-                  Discover University of Melbourne's top talents
+          <Grow in timeout={1100}>
+            <Grid item xs={12} md={6}>
+              <Grid
+                container
+                justify="center"
+                direction="column"
+                alignItems="center"
+              >
+                <Typography>
+                  <br />
+                  <br />
+                  <br />
                 </Typography>
-              </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="h3" align="center">
+                    Discover University of Melbourne's top talents
+                  </Typography>
+                </Grid>
 
-              <Grid item xs={8}>
-                <Typography className={classes.body} variant="h5">
-                  <br />
-                  Microhard makes it easier for students to showcase their
-                  collection of projects and reflections over the course of
-                  their career path.
-                  <br />
-                  <br />
-                </Typography>
-                <Grid container direction="row" justify="center" spacing={3}>
-                  <Grid item xs={6}>
-                    <Grid container justify="center">
-                      <Button
-                        component={Link}
-                        to="/sign-up"
-                        color="primary"
-                        variant="outlined"
-                        size="large"
-                        fullWidth
-                      >
-                        Get started
-                      </Button>
+                <Grid item xs={8}>
+                  <Typography className={classes.body} variant="h5">
+                    <br />
+                    Microhard makes it easier for students to showcase their
+                    collection of projects and reflections over the course of
+                    their career path.
+                    <br />
+                    <br />
+                  </Typography>
+                  <Grid container direction="row" justify="center" spacing={3}>
+                    <Grid item xs={6}>
+                      <Grid container justify="center">
+                        <Button
+                          component={Link}
+                          to="/sign-up"
+                          color="primary"
+                          variant="outlined"
+                          size="large"
+                          fullWidth
+                        >
+                          Get started
+                        </Button>
+                      </Grid>
                     </Grid>
-                  </Grid>
 
-                  <Grid item xs={6}>
-                    <Grid container justify="center">
-                      <Button
-                        component={Link}
-                        to="/sign-in"
-                        color="primary"
-                        variant="contained"
-                        size="large"
-                        fullWidth
-                      >
-                        Login
-                      </Button>
+                    <Grid item xs={6}>
+                      <Grid container justify="center">
+                        <Button
+                          component={Link}
+                          to="/sign-in"
+                          color="primary"
+                          variant="contained"
+                          size="large"
+                          fullWidth
+                        >
+                          Login
+                        </Button>
+                      </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          </Grow>
 
           <Grid item xs={12} md={6}>
             <Typography variant="h1">
               <br />
             </Typography>
-            <Grid container justify="center" alignItems="flex-end">
-              <img src={heroImage} className={classes.image} alt="heroImg" />
-            </Grid>
+            <Grow in timeout={1300}>
+              <Grid container justify="center" alignItems="flex-end">
+                <img src={heroImage} className={classes.image} alt="heroImg" />
+              </Grid>
+            </Grow>
           </Grid>
         </Grid>
       </Container>
