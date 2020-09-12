cf7a8cc45d1c4da775002bf8a88e63b8788b20ff
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-09-12 21:54:27 +0800

    Half way through grid on Account Setting page

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import {updateUser, deleteUser, resetPassword} from '../../actions/userAction';
 import {Helmet} from 'react-helmet';
@@ -7,10 +7,13 @@ import {
   Container,
   Typography,
   Button,
+  Grid,
+  Paper,
 } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
 import {Formik, ErrorMessage, Field, Form} from 'formik';
 import * as Yup from 'yup';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 const validationSchema = Yup.object().shape({
   lastname: Yup.string().required('*Lastname is required'),
@@ -20,8 +23,15 @@ const validationSchema = Yup.object().shape({
   bio: Yup.string(),
 });
 
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+});
+
 class Account extends Component {
   render() {
+    const {classes} = this.props;
     const {error, isAuthenticating, user} = this.props.user;
 
     const deleteAccount = (e) => {
@@ -45,167 +55,181 @@ class Account extends Component {
     }
 
     return (
-      <section>
+      <Fragment>
         <Helmet>
           <title>Microhard &middot; Account</title>
         </Helmet>
 
         <Container>
-          <div className="row">
-            <div className="col">
-              <div className="heading-button">
+          <div className={classes.root}>
+            <Grid container justify="flex-start" direction="row">
+              <Grid item md={12}>
                 <br />
                 <Typography variant="h3">Account</Typography>
-              </div>
-              <hr />
+                <hr />
+                {content}
+                <br />
+              </Grid>
 
-              {content}
-            </div>
+              <Formik
+                initialValues={{
+                  username: user.username,
+                  email: user.email,
+                  bio: user.bio,
+                  lastname: user.lastname,
+                  firstname: user.firstname,
+                  major: user.major,
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                  this.props.dispatch(updateUser({user: values}));
+                }}
+              >
+                {({errors, touched}) => (
+                  <Form>
+                    <Grid
+                      spacing={1}
+                      container
+                      direction="row"
+                      justify="flex-start"
+                    >
+                      <Grid item sm={12} md={6}>
+                        <h3>Basics</h3>
+                        <p>
+                          This information will be shown publicly so be careful
+                          what information you provide.
+                        </p>
+                      </Grid>
+                      <Grid item sm={12} md={6}>
+                        <label htmlFor="firstname">First name</label>
+                        <Field
+                          name="firstname"
+                          placeholder="Enter your firstname"
+                          className={`form-control ${
+                            touched.firstname && errors.firstname
+                              ? 'is-invalid'
+                              : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="firstname"
+                          className="invalid-feedback"
+                        />
+                      </Grid>
+                    </Grid>
+                  </Form>
+                )}
+              </Formik>
+            </Grid>
           </div>
+        </Container>
 
-          <Formik
-            initialValues={{
-              username: user.username,
-              email: user.email,
-              bio: user.bio,
-              lastname: user.lastname,
-              firstname: user.firstname,
-              major: user.major,
-            }}
-            validationSchema={validationSchema}
-            onSubmit={(values) => {
-              this.props.dispatch(updateUser({user: values}));
-            }}
-          >
-            {({errors, touched}) => (
-              <Form>
-                <div className="row">
-                  <div className="col-md-4">
-                    <h3 className="form-info-title">Basics</h3>
-                    <p className="form-info-text">
-                      This information will be shown publicly so be careful what
-                      information you provide.
-                    </p>
-                  </div>
-
-                  <div className="col">
-                    <div className="form-card">
-                      <div className="form-content">
-                        <div className="form-group">
-                          <label htmlFor="firstname">First name</label>
-                          <Field
-                            name="firstname"
-                            placeholder="Enter your firstname"
-                            className={`form-control ${
-                              touched.firstname && errors.firstname
-                                ? 'is-invalid'
-                                : ''
-                            }`}
-                          />
-                          <ErrorMessage
-                            component="div"
-                            name="firstname"
-                            className="invalid-feedback"
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="lastname">Last Name</label>
-                          <Field
-                            name="lastname"
-                            placeholder="Enter your lastname"
-                            className={`form-control ${
-                              touched.lastname && errors.lastname
-                                ? 'is-invalid'
-                                : ''
-                            }`}
-                          />
-                          <ErrorMessage
-                            component="div"
-                            name="lastname"
-                            className="invalid-feedback"
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="username">Username</label>
-                          <Field
-                            name="username"
-                            placeholder="Enter your username"
-                            className={`form-control ${
-                              touched.username && errors.username
-                                ? 'is-invalid'
-                                : ''
-                            }`}
-                          />
-                          <ErrorMessage
-                            component="div"
-                            name="username"
-                            className="invalid-feedback"
-                          />
-                        </div>
-
-                        <div className="form-group">
-                          <label htmlFor="email">Email</label>
-                          <Field
-                            name="email"
-                            type="email"
-                            placeholder="Enter your email"
-                            className={`form-control ${
-                              touched.email && errors.email ? 'is-invalid' : ''
-                            }`}
-                          />
-                          <ErrorMessage
-                            component="div"
-                            name="email"
-                            className="invalid-feedback"
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="Major">Major</label>
-                          <Field
-                            name="major"
-                            placeholder="Enter your major"
-                            className={`form-control ${
-                              touched.major && errors.major ? 'is-invalid' : ''
-                            }`}
-                          />
-                          <ErrorMessage
-                            component="div"
-                            name="major"
-                            className="invalid-feedback"
-                          />
-                        </div>
-
-                        <div className="form-group">
-                          <label htmlFor="bio">Bio</label>
-                          <Field
-                            as="textarea"
-                            name="bio"
-                            placeholder="Describe yourself"
-                            className={`form-control ${
-                              touched.bio && errors.bio ? 'is-invalid' : ''
-                            }`}
-                          />
-                          <ErrorMessage
-                            component="div"
-                            name="bio"
-                            className="invalid-feedback"
-                          />
-                        </div>
+        <Container>
+          {({errors, touched}) => (
+            <Form>
+              <div className="row">
+                <div className="col">
+                  <div className="form-card">
+                    <div className="form-content">
+                      <div className="form-group"></div>
+                      <div className="form-group">
+                        <label htmlFor="lastname">Last Name</label>
+                        <Field
+                          name="lastname"
+                          placeholder="Enter your lastname"
+                          className={`form-control ${
+                            touched.lastname && errors.lastname
+                              ? 'is-invalid'
+                              : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="lastname"
+                          className="invalid-feedback"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <Field
+                          name="username"
+                          placeholder="Enter your username"
+                          className={`form-control ${
+                            touched.username && errors.username
+                              ? 'is-invalid'
+                              : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="username"
+                          className="invalid-feedback"
+                        />
                       </div>
 
-                      <div className="form-footer text-right">
-                        <button type="submit" className="btn btn-primary">
-                          Update settings
-                        </button>
+                      <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <Field
+                          name="email"
+                          type="email"
+                          placeholder="Enter your email"
+                          className={`form-control ${
+                            touched.email && errors.email ? 'is-invalid' : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="email"
+                          className="invalid-feedback"
+                        />
                       </div>
+                      <div className="form-group">
+                        <label htmlFor="Major">Major</label>
+                        <Field
+                          name="major"
+                          placeholder="Enter your major"
+                          className={`form-control ${
+                            touched.major && errors.major ? 'is-invalid' : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="major"
+                          className="invalid-feedback"
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="bio">Bio</label>
+                        <Field
+                          as="textarea"
+                          name="bio"
+                          placeholder="Describe yourself"
+                          className={`form-control ${
+                            touched.bio && errors.bio ? 'is-invalid' : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="bio"
+                          className="invalid-feedback"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="form-footer text-right">
+                      <button type="submit" className="btn btn-primary">
+                        Update settings
+                      </button>
                     </div>
                   </div>
                 </div>
+              </div>
 
-                <hr />
-              </Form>
-            )}
-          </Formik>
+              <hr />
+            </Form>
+          )}
 
           <Formik
             initialValues={{
@@ -312,7 +336,7 @@ class Account extends Component {
           </div>
         </Container>
         <br />
-      </section>
+      </Fragment>
     );
   }
 }
@@ -321,4 +345,4 @@ const mapStateToProps = (state) => ({
   ...state,
 });
 
-export default connect(mapStateToProps)(Account);
+export default connect(mapStateToProps)(withStyles(styles)(Account));
