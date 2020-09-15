401066f0118d0faf52f8fcd8aa26d38ecd2b95f5
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-09-16 00:05:36 +0700

    add a very simple get image case test

---
+++ test/test.js
@@ -0,0 +1,39 @@
+process.env.NODE_ENV = 'test';
+
+const server = require('../server.js');
+const request = require('supertest');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('User', () => {
+    var token = '';
+    var id = '';
+
+    before(function (done) {
+        request(server)
+            .post('/api/user/sign-in')
+            .send({
+                user: {
+                    email: 'admin@gmail.com',
+                    password: 'admin',
+                },
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+                token = res.body.token;
+                done();
+            });
+    });
+
+    it('should get all of the user images', () =>
+        request(server)
+            .get('/api/image')
+            .set('Authorization', 'Bearer ' + token)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then((r) => {
+                expect(r.body.files).to.be.an.an('array');
+            }));
+
+});
