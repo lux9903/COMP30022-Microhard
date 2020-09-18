760993b2310d97d247f19b32dbf08333b39a24b5
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-09-18 18:07:01 +0800

    Preview pdf files

--- client/src/components/Dashboard/Image.jsx
+++ client/src/components/Dashboard/Image.jsx
@@ -1,28 +1,140 @@
-import React, {Component, Fragment} from 'react';
+// import React, {Component, Fragment} from 'react';
+// import {Helmet} from 'react-helmet';
+//
+// import ReactDOM from 'react-dom';
+// import axios from '../../helpers/axiosConfig';
+// import {Container} from '@material-ui/core';
+// import Grid from '@material-ui/core/Grid';
+// import withStyles from '@material-ui/core/styles/withStyles';
+// import Typography from '@material-ui/core/Typography';
+// import Input from '@material-ui/core/Input';
+// import Button from '@material-ui/core/Button';
+//
+// const styles = (theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+// });
+//
+// class FilesUploadComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       file: null,
+//     };
+//     this.onFormSubmit = this.onFormSubmit.bind(this);
+//     this.onChange = this.onChange.bind(this);
+//   }
+//   onFormSubmit(e) {
+//     e.preventDefault();
+//     const formData = new FormData();
+//     formData.append('file', this.state.file);
+//     const config = {
+//       headers: {
+//         'content-type': 'multipart/form-data',
+//       },
+//     };
+//     axios
+//       .post('/image/upload', formData, config)
+//       .then((response) => {
+//         alert('The file is successfully uploaded');
+//       })
+//       .catch((error) => {});
+//   }
+//   onChange(e) {
+//     this.setState({file: e.target.files[0]});
+//   }
+//
+//   componentDidMount() {
+//     const imgs = axios.get('/image').then((res) => {
+//       if (res.data.files) {
+//         const imgPic = res.data.files.map((ele) => (
+//           <img
+//             src={'/api/image/' + ele.filename}
+//             alt={'/image/' + ele.filename}
+//           />
+//         ));
+//         ReactDOM.render(imgPic, document.getElementById('all_img'));
+//       }
+//     });
+//   }
+//
+//   render() {
+//     const {classes} = this.props;
+//     return (
+//       <Fragment>
+//         <div style={{height: '200px', backgroundColor: '#094183'}}>
+//           <br />
+//           <br />
+//           <br />
+//           <h1
+//             align="center"
+//             style={{color: '#fff', fontFamily: 'Nunito, sans-serif'}}
+//           >
+//             Upload image
+//           </h1>
+//         </div>
+//
+//         <div className={classes.root}>
+//           <Container>
+//             <Helmet>
+//               <title>Microhard &middot; Upload Image </title>
+//             </Helmet>
+//
+//             <Grid
+//               container
+//               direction="column"
+//               justify="center"
+//               alignItems="center"
+//               alignContent="center"
+//             >
+//               <Grid item xs={12} sm={12} md={12}>
+//                 <div style={{padding: '20px'}}>
+//                   <form onSubmit={this.onFormSubmit}>
+//                     <Input
+//                       type="file"
+//                       name="file"
+//                       onChange={this.onChange}
+//                       color="primary"
+//                     />
+//                     <Button type="submit" color="primary" variant="contained">
+//                       Upload
+//                     </Button>
+//                   </form>
+//                 </div>
+//               </Grid>
+//               <br />
+//               <div id="all_img" align="center"></div>
+//             </Grid>
+//           </Container>
+//         </div>
+//       </Fragment>
+//     );
+//   }
+// }
+//
+// export default withStyles(styles)(FilesUploadComponent);
+
+import React, {Component} from 'react';
 import {Helmet} from 'react-helmet';
+import {Link} from 'react-router-dom';
 
 import ReactDOM from 'react-dom';
+//import axios from 'axios';
 import axios from '../../helpers/axiosConfig';
-import {Container} from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
+import MaterialTable from 'material-table';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 
-const styles = (theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-});
-
-class FilesUploadComponent extends Component {
+export default class FilesUploadComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       file: null,
     };
+    this.ondelete = this.ondelete.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onFormSubmitPDF = this.onFormSubmitPDF.bind(this);
     this.onChange = this.onChange.bind(this);
   }
   onFormSubmit(e) {
@@ -41,8 +153,39 @@ class FilesUploadComponent extends Component {
       })
       .catch((error) => {});
   }
+  onFormSubmitPDF(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.file);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios
+      .post('/pdf/upload', formData, config)
+      .then((response) => {
+        alert('The file is successfully uploaded');
+      })
+      .catch((error) => {});
+  }
   onChange(e) {
     this.setState({file: e.target.files[0]});
+  }
+  ondelete(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios
+      .delete('/pdf/5f57882947ad6155d8f36837')
+      .then((response) => {
+        alert('deleted');
+      })
+      .catch((error) => {});
   }
 
   componentDidMount() {
@@ -57,60 +200,44 @@ class FilesUploadComponent extends Component {
         ReactDOM.render(imgPic, document.getElementById('all_img'));
       }
     });
+
+    const pdf = axios.get('/pdf').then((res) => {
+      if (res.data.files) {
+        const pdf = res.data.files.map((ele) => (
+          <ul>
+            <li>
+              <a href={'/api/pdf/' + ele.filename}>{ele.filename}</a>
+            </li>
+          </ul>
+        ));
+        ReactDOM.render(pdf, document.getElementById('all_pdf'));
+      }
+    });
   }
 
   render() {
-    const {classes} = this.props;
     return (
-      <Fragment>
-        <div style={{height: '200px', backgroundColor: '#094183'}}>
-          <br />
-          <br />
-          <br />
-          <h1
-            align="center"
-            style={{color: '#fff', fontFamily: 'Nunito, sans-serif'}}
-          >
-            Upload image
-          </h1>
-        </div>
+      <div className="container">
+        <div className="row">
+          <div>
+            <h3>React File Upload</h3>
+          </div>
+          <div>
+            <form onSubmit={this.onFormSubmit}>
+              <input type="file" name="file" onChange={this.onChange} />
+              <button type="submit">Upload</button>
+            </form>
 
-        <div className={classes.root}>
-          <Container>
-            <Helmet>
-              <title>Microhard &middot; Upload Image </title>
-            </Helmet>
-
-            <Grid
-              container
-              direction="column"
-              justify="center"
-              alignItems="center"
-              alignContent="center"
-            >
-              <Grid item xs={12} sm={12} md={12}>
-                <div style={{padding: '20px'}}>
-                  <form onSubmit={this.onFormSubmit}>
-                    <Input
-                      type="file"
-                      name="file"
-                      onChange={this.onChange}
-                      color="primary"
-                    />
-                    <Button type="submit" color="primary" variant="contained">
-                      Upload
-                    </Button>
-                  </form>
-                </div>
-              </Grid>
-              <br />
-              <div id="all_img" align="center"></div>
-            </Grid>
-          </Container>
+            <form onSubmit={this.onFormSubmitPDF}>
+              <input type="file" name="file" onChange={this.onChange} />
+              <button type="submit">Upload</button>
+            </form>
+          </div>
+          {/*<div id="all_img"></div>*/}
+          <button onClick={this.ondelete}> test Delete</button>
+          <div id="all_pdf"></div>
         </div>
-      </Fragment>
+      </div>
     );
   }
 }
-
-export default withStyles(styles)(FilesUploadComponent);
--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -7,72 +7,72 @@ const mongoose = require('mongoose');
 
 const conn = mongoose.createConnection(process.env.DATABASE);
 let gfs;
-conn.once('open',() => {
-  //Init stream
+conn.once('open', () => {
+  // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
-})
+});
 
 const storage = new GridFsStorage({
-    url: process.env.DATABASE,
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        crypto.randomBytes(16, (err, buf) => {
-          if (err) {
-            return reject(err);
-          }
-          const filename = buf.toString('hex') + path.extname(file.originalname);
-          const fileInfo = {
-            filename: filename,
-            bucketName: 'uploads'
-          };
-          resolve(fileInfo);
-        });
+  url: process.env.DATABASE,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename,
+          bucketName: 'uploads',
+        };
+        resolve(fileInfo);
       });
-    }
-  });
-const upload = multer({ storage });
+    });
+  },
+});
+const upload = multer({storage});
 
-//get all files
-const getAllFlie = (req, res) =>{
-  gfs.files.find().toArray((err,files)=>{
-    //check if files
-    if(!files || files.length === 0){
+// get all files
+const getAllFile = (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // check if files
+    if (!files || files.length === 0) {
       return res.status(404).json({
-        err: 'No files exist'
+        err: 'No files exist',
       });
     }
 
     res.json(files);
   });
-}
+};
 
-//get one specific file
-const getOneFile = (req, res) =>{
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+// get one specific file
+const getOneFile = (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
     // Check if file
     if (!file || file.length === 0) {
       return res.status(404).json({
-        err: 'No file exists'
+        err: 'No file exists',
       });
     }
     // File exists
     return res.json(file);
   });
-}
+};
 
 // delete one file by file id
-const deleteOneFile = (req, res) =>{
-  gfs.remove({_id:req.params.id, root: 'uploads'}, (err, gridsStore) =>{
-    if(err){
-      return res.status(404).json({err:err});
+const deleteOneFile = (req, res) => {
+  gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridsStore) => {
+    if (err) {
+      return res.status(404).json({err});
     }
 
-    return res.send("Deleted"+req.params.id);
+    return res.send(`Deleted${req.params.id}`);
   });
-}
+};
 module.exports = {
-  getAllFlie,
+  getAllFile,
   getOneFile,
   deleteOneFile,
-}
+};
