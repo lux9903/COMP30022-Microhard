3bc26130603becfbb5bb53e6bbec11694d56d3a2
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-09-24 22:32:20 +0800

    More compression of images

--- client/src/components/Dashboard/Project/Project.jsx
+++ client/src/components/Dashboard/Project/Project.jsx
@@ -10,7 +10,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import img from './form-background.jpg';
+import img from '../../Account/form-background.jpg';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
