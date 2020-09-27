11cecbb29b6c7f7be1a61d784db6a3c6b3ec7cf6
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-09-28 00:32:20 +0700

    add avatar to profile page

--- client/src/components/Dashboard/Image.jsx
+++ client/src/components/Dashboard/Image.jsx
@@ -9,51 +9,12 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import PopupState, {bindPopover, bindTrigger} from 'material-ui-popup-state';
-import IconButton from '@material-ui/core/IconButton';
-import Popover from '@material-ui/core/Popover';
-import MenuItem from '@material-ui/core/MenuItem';
-import {Link} from 'react-router-dom';
-import {signOutUser} from '../../actions/userAction';
-import EditAvatar from './EditAvatar';
 
 
 const styles = (theme) => ({
   root: {
     flexGrow: 1,
-  },
-  large: {
-    width: theme.spacing(18),
-    height: theme.spacing(18),
-  },
-  button: {
-    color: 'grey',
-    margin: '0px 20px',
-    textTransform: 'none',
-  },
-  logo: {
-    maxHeight: '2.7rem',
-    padding: '0px 10px',
-    margin: '0px 20px',
-  },
-  buttonSection: {
-    flex: '1',
-    textAlign: 'center',
-    marginLeft: '-43px',
-  },
-  appbar: {
-    backgroundColor: '#F4F5F7',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  noDecoration: {
-    textDecoration: 'none !important',
-  },
+  }
 });
 
 class FilesUploadComponent extends Component {
@@ -75,7 +36,7 @@ class FilesUploadComponent extends Component {
       },
     };
     axios
-      .post('/avatar/upload', formData, config)
+      .post('/image/upload', formData, config)
       .then((response) => {
         alert('The file is successfully uploaded');
       })
@@ -86,14 +47,12 @@ class FilesUploadComponent extends Component {
   }
 
   componentDidMount() {
-    const {classes} = this.props;
-    const imgs = axios.get('/avatar').then((res) => {
+    const imgs = axios.get('/image').then((res) => {
       if (res.data.files) {
         const imgPic = res.data.files.map((ele) => (
-          <Avatar
+          <img
               alt="Nothing Here"
               src={'/api/image/' + ele.filename}
-              className={classes.large}
           />
         ));
         ReactDOM.render(imgPic, document.getElementById('all_img'));
@@ -141,43 +100,8 @@ class FilesUploadComponent extends Component {
                   </form>
                 </div>
               </Grid>
-              <div> Hello </div>
               <br />
-              <div>
-                <PopupState variant="popover" popupId="demo-popup-popover">
-                  {(popupState) => (
-                      <div>
-                        <IconButton
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            color="inherit"
-                        >
-                          <div id="all_img" align="center" {...bindTrigger(popupState)} ></div>
-                        </IconButton>
-                        <Popover
-                            {...bindPopover(popupState)}
-                            anchorOrigin={{
-                              vertical: 'bottom',
-                              horizontal: 'center',
-                            }}
-                            transformOrigin={{
-                              vertical: 'top',
-                              horizontal: 'center',
-                            }}
-                        >
-                          <MenuItem component={Link} to="/">
-                            View Profile Picture
-                          </MenuItem>
-                          <EditAvatar
-                              onFormSubmit={this.onFormSubmit}
-                              onChange={this.onChange}
-                          />
-                        </Popover>
-                      </div>
-                  )}
-                </PopupState>
-              </div>
+              <div id="all_img"></div>
             </Grid>
           </Container>
         </div>
--- client/src/components/Dashboard/Profile/Profile.jsx
+++ client/src/components/Dashboard/Profile/Profile.jsx
@@ -14,6 +14,11 @@ import AttachFileIcon from '@material-ui/icons/AttachFile';
 import {ReactPhotoCollage} from 'react-photo-collage';
 import {Link} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import PopupState, {bindPopover, bindTrigger} from 'material-ui-popup-state';
+import Popover from '@material-ui/core/Popover';
+import MenuItem from '@material-ui/core/MenuItem';
+import EditAvatar from '../EditAvatar';
 
 const styles = (theme) => ({
   root: {
@@ -94,10 +99,70 @@ const styles = (theme) => ({
     fontFamily: 'Lato, sans-serif',
     fontWeight: '300',
   },
+  large: {
+    width: theme.spacing(18),
+    height: theme.spacing(18),
+  },
+  button: {
+    color: 'grey',
+    margin: '0px 20px',
+    textTransform: 'none',
+  },
+  logo: {
+    maxHeight: '2.7rem',
+    padding: '0px 10px',
+    margin: '0px 20px',
+  },
+  buttonSection: {
+    flex: '1',
+    textAlign: 'center',
+    marginLeft: '-43px',
+  },
+  appbar: {
+    backgroundColor: '#F4F5F7',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  noDecoration: {
+    textDecoration: 'none !important',
+  },
 });
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  onFormSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.file);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios
+        .post('/avatar/upload', formData, config)
+        .then((response) => {
+          alert('The file is successfully uploaded');
+        })
+        .catch((error) => {});
+  }
+  onChange(e) {
+    this.setState({file: e.target.files[0]});
+  }
+
   componentDidMount() {
+    const {classes} = this.props;
     const imgs = axios.get('/image').then((res) => {
       if (res.data.files) {
         //const imgPic = res.data.files.map((ele) => src={"/api/image/"+ele.filename} alt={"/image/"+ele.filename} />);
@@ -132,6 +197,18 @@ class Profile extends Component {
         ReactDOM.render(photogrid, document.getElementById('all_img'));
       }
     });
+    const img = axios.get('/avatar').then((res) => {
+      if (res.data.files) {
+        const imgPic = res.data.files.map((ele) => (
+            <Avatar
+                alt="Nothing Here"
+                src={'/api/image/' + ele.filename}
+                className={classes.large}
+            />
+        ));
+        ReactDOM.render(imgPic, document.getElementById('avatar'));
+      }
+    });
   }
 
   render() {
@@ -153,7 +230,39 @@ class Profile extends Component {
                 <Grid item xs={12} sm={12} md={12}>
                   <div className={classes.profile}>
                     <div>
-                      <Gravatar email={user.email} size={'2048px'} />
+                        <PopupState variant="popover" popupId="demo-popup-popover">
+                          {(popupState) => (
+                              <div>
+                                <IconButton
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    color="inherit"
+                                >
+                                  <div id="avatar" align="center" {...bindTrigger(popupState)} ></div>
+                                </IconButton>
+                                <Popover
+                                    {...bindPopover(popupState)}
+                                    anchorOrigin={{
+                                      vertical: 'bottom',
+                                      horizontal: 'center',
+                                    }}
+                                    transformOrigin={{
+                                      vertical: 'top',
+                                      horizontal: 'center',
+                                    }}
+                                >
+                                  <MenuItem component={Link} to="/">
+                                    View Profile Picture
+                                  </MenuItem>
+                                  <EditAvatar
+                                      onFormSubmit={this.onFormSubmit}
+                                      onChange={this.onChange}
+                                  />
+                                </Popover>
+                              </div>
+                          )}
+                        </PopupState>
                     </div>
                     <div style={{marginTop: '-60px'}}>
                       <Typography variant="h3" className={classes.fullName}>
