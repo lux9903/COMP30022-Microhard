94c3231fe04506ed2770fb728860c74f1843c6c0
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-09-28 00:18:51 +0700

    udpate update function

--- server/routes/avatarRouter.js
+++ server/routes/avatarRouter.js
@@ -6,6 +6,14 @@ const imageController = require('../controllers/imageController');
 const Image = mongoose.model('Image');
 const User = mongoose.model('User');
 const upload = imageController.upload;
+const conn = mongoose.createConnection(process.env.DATABASE);
+const Grid = require('gridfs-stream');
+let gfs;
+conn.once('open',() => {
+    //Init stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+})
 
 // : /image...
 
@@ -15,6 +23,18 @@ const upload = imageController.upload;
 avatarRouter.post('/upload',imageController.upload.single('file'),auth.optional, (req,res) => {
     User.findById(req.payload.id)
         .then(function (user) {
+            Image.find({user: user._id, type: 'avatar'}).distinct('fileId').then(function(image){
+                Image.deleteOne({user: user._id, fileId: image}, (err)=> {
+                    if(err){
+                        return res.status(404).json({err:"Relation does not exist"});
+                    }
+                    gfs.remove({_id:image, root: 'uploads'}, (err, gridsStore) =>{
+                        if(err){
+                            return res.status(404).json({err:err});
+                        }
+                    });
+                });
+            })
             const image = new Image(req.body);
             image.filename = req.file.filename;
             image.originalName = req.file.originalname;
