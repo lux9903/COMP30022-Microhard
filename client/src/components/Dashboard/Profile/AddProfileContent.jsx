8b7134f37e7af9f9d97929b1099618f3b9fe464e
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-09-28 23:00:52 +0700

    connect stepper to backend

--- client/src/components/Dashboard/Profile/AddProfileContent.jsx
+++ client/src/components/Dashboard/Profile/AddProfileContent.jsx
@@ -1,45 +1,67 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import {FormWizard} from 'react-material-formik-wizard';
 import AboutSectionStep from './AboutSectionStep';
 import BasicDetailsStep from './BasicDetailsStep';
 import ReviewStep from './ReviewStep';
+import {signUpUser} from '../../../actions/userAction';
+import Alert from '@material-ui/lab/Alert';
+import {connect} from 'react-redux';
+import {CircularProgress,} from '@material-ui/core';
+class AddProfileContent extends Component {
 
-function AddProfileContent() {
-  const steps = [
-    {
-      component: BasicDetailsStep,
-      title: 'Basic details',
-    },
-    {
-      component: AboutSectionStep,
-      title: 'About',
-    },
-    {
-      component: ReviewStep,
-      title: 'Review',
-    },
-  ];
 
-  const doSubmit = (values) => {
-    alert('submitting: ' + JSON.stringify(values));
-    console.log('submitting values', values);
-  };
+  render()
+  {
+    const steps = [
+      {
+        component: BasicDetailsStep,
+        title: 'Basic details',
+      },
+      {
+        component: AboutSectionStep,
+        title: 'About',
+      },
+      {
+        component: ReviewStep,
+        title: 'Review',
+      },
+    ];
 
-  return (
-    <React.Fragment>
-      <main>
-        <FormWizard
-          displayProgress={true}
-          formComponents={steps}
-          doSubmit={doSubmit}
-          successTitle={'Success'}
-          successTitleComponent={'h1'}
-          successMessage={'Your user profile has been successfully made!'}
-          successMessageComponent={'h5'}
-        />
-      </main>
-    </React.Fragment>
-  );
+    const doSubmit = (values) => {
+      this.props.dispatch(signUpUser({user: values}));
+    };
+    const {error, isAuthenticating} = this.props.user;
+
+    let content;
+
+    if (error) {
+      content = <Alert severity="error">{error}</Alert>;
+    } else if (isAuthenticating) {
+      content = (
+          <CircularProgress>
+            <span>Loading...</span>
+          </CircularProgress>
+      );
+    }
+    return (
+        <React.Fragment>
+          <main>
+            <FormWizard
+                displayProgress={true}
+                formComponents={steps}
+                doSubmit={doSubmit}
+                successTitle={'Success'}
+                successTitleComponent={'h1'}
+                successMessage={'Your user profile has been successfully made!'}
+                successMessageComponent={'h5'}
+            />
+          </main>
+        </React.Fragment>
+    );
+  }
 }
+const mapStateToProps = (state) => ({
+  ...state,
+});
 
-export default AddProfileContent;
+export default connect(mapStateToProps)(AddProfileContent);
