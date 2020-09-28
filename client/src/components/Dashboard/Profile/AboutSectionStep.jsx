4dad09a4e13934fff30d52e8b50eb05a129a21a9
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-09-28 12:41:35 +0800

    Finish AboutSectionStep.jsx

--- client/src/components/Dashboard/Profile/AboutSectionStep.jsx
+++ client/src/components/Dashboard/Profile/AboutSectionStep.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {Formik} from 'formik';
 import {Button, Grid, Typography} from '@material-ui/core';
 import * as Yup from 'yup';
@@ -9,7 +9,9 @@ import * as Yup from 'yup';
 import {FormItem} from 'react-material-formik-wizard';
 
 const AboutSchema = Yup.object().shape({
-  description: Yup.string().trim(),
+  major: Yup.string().trim().max(60, 'Too long! Character limit is 60'),
+  headline: Yup.string().trim().max(60, 'Too long! Character limit is 60'),
+  aboutSection: Yup.string().trim(),
 });
 
 const AboutSectionStep = (props) => {
@@ -18,6 +20,8 @@ const AboutSectionStep = (props) => {
   useEffect(() => {
     const mapValues = () => {
       values.aboutSection = values.aboutSection ? values.aboutSection : '';
+      values.major = values.major ? values.major : '';
+      values.headline = values.headline ? values.headline : '';
     };
     mapValues();
   }, []);
@@ -28,11 +32,15 @@ const AboutSectionStep = (props) => {
         enableReinitialize={true}
         initialValues={{
           aboutSection: values.aboutSection,
+          major: values.major,
+          headline: values.headline,
         }}
         validationSchema={AboutSchema}
         onSubmit={(values) => {
           next({
             aboutSection: values.aboutSection,
+            major: values.major,
+            headline: values.headline,
           });
         }}
       >
@@ -55,7 +63,39 @@ const AboutSectionStep = (props) => {
               >
                 About
               </Typography>
-              <Grid item xs={12}>
+              <Grid item xs={12} style={{padding: '15px 0px'}}>
+                <FormItem
+                  id="headline"
+                  name="headline"
+                  type="text"
+                  label="Headline"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder={
+                    '2nd Year Veterinary Medicine undergraduate student'
+                  }
+                  value={values.headline}
+                  error={errors.headline}
+                  touched={touched.headline}
+                />
+              </Grid>
+              <Grid item xs={12} style={{padding: '15px 0px'}}>
+                <FormItem
+                  id="major"
+                  name="major"
+                  type="text"
+                  label="Major"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder={
+                    'Computer Science / Architecture / History? Diploma in Languages?'
+                  }
+                  value={values.major}
+                  error={errors.major}
+                  touched={touched.major}
+                />
+              </Grid>
+              <Grid item xs={12} style={{padding: '15px 0px'}}>
                 <FormItem
                   id="aboutSection"
                   name="aboutSection"
