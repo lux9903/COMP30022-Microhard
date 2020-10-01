82ddcdb65ab43a6e9fae212eccb89a7c90df4be4
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-10-02 00:36:30 +0800

    Remove repeated code for ImageGrid in Profile.jsx

--- client/src/components/Dashboard/Profile/Profile.jsx
+++ client/src/components/Dashboard/Profile/Profile.jsx
@@ -20,7 +20,7 @@ import SchoolIcon from '@material-ui/icons/School';
 import PublicIcon from '@material-ui/icons/Public';
 import Grow from '@material-ui/core/Grow';
 import PDFPreview from './PDFPreview';
-import Gallery from 'react-grid-gallery';
+import ImageGrid from '../ImageGrid';
 
 const styles = (theme) => ({
   root: {
@@ -100,31 +100,11 @@ class Profile extends Component {
     super(props);
     this.state = {
       file: null,
-      images: null,
-      deleteImageLink: null,
-      currentImage: 0,
     };
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
-
-    this.onCurrentImageChange = this.onCurrentImageChange.bind(this);
-    this.deleteImage = this.deleteImage.bind(this);
   }
 
-  onCurrentImageChange(index) {
-    this.setState({currentImage: index});
-  }
-
-  deleteImage() {
-    if (
-      window.confirm(
-        `Are you sure you want to delete image number ${this.state.currentImage}?`
-      )
-    ) {
-      axios.delete(this.state.deleteImageLink[this.state.currentImage]);
-      window.location.reload();
-    }
-  }
   onFormSubmit(e) {
     e.preventDefault();
     const formData = new FormData();
@@ -147,51 +127,6 @@ class Profile extends Component {
 
   componentDidMount() {
     const {classes} = this.props;
-    const imgs = axios.get('/image').then((res) => {
-      if (res.data.files) {
-        //const imgPic = res.data.files.map((ele) => src={"/api/image/"+ele.filename} alt={"/image/"+ele.filename} />);
-        const photodata = res.data.files.map(getPhoto);
-        function getPhoto(elem) {
-          return {
-            src: '/api/image/' + elem.filename,
-            thumbnail: '/api/image/' + elem.filename,
-            thumbnailWidth: 320,
-            thumbnailHeight: 213,
-          };
-        }
-        this.setState({images: photodata});
-        const deleteLink = res.data.files.map((ele) => '/image/' + ele._id);
-        this.setState({deleteImageLink: deleteLink});
-
-        let photogrid = (
-          <Container>
-            <div
-              style={{
-                display: 'block',
-                minHeight: '1px',
-                width: '100%',
-                border: '1px solid #ddd',
-                overflow: 'auto',
-              }}
-            >
-              <Gallery
-                maxRows={5}
-                images={photodata}
-                enableLightbox={true}
-                enableImageSelection={false}
-                currentImageWillChange={this.onCurrentImageChange}
-                customControls={[
-                  <button key="deleteImage" onClick={this.deleteImage}>
-                    Delete Image
-                  </button>,
-                ]}
-              />
-            </div>
-          </Container>
-        );
-        ReactDOM.render(photogrid, document.getElementById('all_img'));
-      }
-    });
     // Retrieve avatar image
     const img = axios.get('/avatar').then((res) => {
       if (res.data.files) {
@@ -375,7 +310,7 @@ class Profile extends Component {
                   <Typography variant="h2">Photos</Typography>
                 </Grid>
                 <Grid item xs={12} sm={11} md={11}>
-                  <div id="all_img"></div>
+                  <ImageGrid />
                 </Grid>
               </Grid>
               <Grid
