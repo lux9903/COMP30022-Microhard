33d94011dbb149477c1b8801ca0de059d09af9cc
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-10-02 00:32:02 +0800

    Broke down uploadImage and ImageGrid components

--- client/src/components/Dashboard/Image.jsx
+++ client/src/components/Dashboard/Image.jsx
@@ -1,6 +1,5 @@
 import React, {Component, Fragment} from 'react';
 import {Helmet} from 'react-helmet';
-
 import ReactDOM from 'react-dom';
 import axios from '../../helpers/axiosConfig';
 import {Container} from '@material-ui/core';
@@ -9,8 +8,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
-import Gallery from 'react-grid-gallery';
-import clsx from 'clsx';
+import ImageGrid from './ImageGrid';
 
 const styles = (theme) => ({
   root: {
@@ -18,7 +16,8 @@ const styles = (theme) => ({
   },
 });
 
-class FilesUploadComponent extends Component {
+// Photo page that contains upload button to upload and preview images in grid
+class Image extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,8 +27,6 @@ class FilesUploadComponent extends Component {
     };
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
-    this.deleteImage = this.deleteImage.bind(this);
-    this.onCurrentImageChange = this.onCurrentImageChange.bind(this);
   }
   onFormSubmit(e) {
     e.preventDefault();
@@ -49,67 +46,6 @@ class FilesUploadComponent extends Component {
   }
   onChange(e) {
     this.setState({file: e.target.files[0]});
-  }
-  deleteImage() {
-    if (
-      window.confirm(
-        `Are you sure you want to delete image number ${this.state.currentImage}?`
-      )
-    ) {
-      axios.delete(this.state.deleteImageLink[this.state.currentImage]);
-      window.location.reload();
-    }
-  }
-  onCurrentImageChange(index) {
-    this.setState({currentImage: index});
-  }
-
-  componentDidMount() {
-    const imgs = axios.get('/image').then((res) => {
-      if (res.data.files) {
-        //const imgPic = res.data.files.map((ele) => src={"/api/image/"+ele.filename} alt={"/image/"+ele.filename} />);
-        const photodata = res.data.files.map(getPhoto);
-        function getPhoto(elem) {
-          return {
-            src: '/api/image/' + elem.filename,
-            thumbnail: '/api/image/' + elem.filename,
-            thumbnailWidth: 340,
-            thumbnailHeight: 250,
-          };
-        }
-        this.setState({images: photodata});
-        const deleteLink = res.data.files.map((ele) => '/image/' + ele._id);
-        this.setState({deleteImageLink: deleteLink});
-
-        let photogrid = (
-          <Container>
-            <div
-              style={{
-                display: 'block',
-                minHeight: '1px',
-                width: '100%',
-                border: '1px solid #ddd',
-                overflow: 'auto',
-              }}
-            >
-              <Gallery
-                maxRows={5}
-                images={photodata}
-                enableLightbox={true}
-                enableImageSelection={false}
-                currentImageWillChange={this.onCurrentImageChange}
-                customControls={[
-                  <button key="deleteImage" onClick={this.deleteImage}>
-                    Delete Image
-                  </button>,
-                ]}
-              />
-            </div>
-          </Container>
-        );
-        ReactDOM.render(photogrid, document.getElementById('all_img'));
-      }
-    });
   }
 
   render() {
@@ -155,7 +91,7 @@ class FilesUploadComponent extends Component {
               </Grid>
               <br />
             </Grid>
-            <div id="all_img"></div>
+            <ImageGrid />
           </Container>
         </div>
       </Fragment>
@@ -163,4 +99,4 @@ class FilesUploadComponent extends Component {
   }
 }
 
-export default withStyles(styles)(FilesUploadComponent);
+export default withStyles(styles)(Image);
