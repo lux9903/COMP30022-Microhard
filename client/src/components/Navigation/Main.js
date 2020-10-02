893a47a881d17b54332c745008d0275bf366c89b
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-10-02 21:07:24 +0800

    Revert "Merge pull request #57 from lux9903/feature/resume-2"
    This reverts commit 80e70ba1f21635e6bc52de052db96bf20b7b28da, reversing
    changes made to 7c6ca2d406c64c386d190792c7eece845217daae.

--- client/src/components/Navigation/Main.js
+++ client/src/components/Navigation/Main.js
@@ -9,13 +9,15 @@ import SignIn from '../Account/SignIn';
 import ForgotPassword from '../Account/ForgotPassword';
 import ResetPassword from '../Account/ResetPassword';
 import Experience from '../Dashboard/Experience';
-import Image from '../Dashboard/Photos/Image';
+import Image from '../Dashboard/Image';
 import Project from '../Dashboard/Project/ProjectList_edit';
 import Project_Edit from '../Dashboard/Project/Project_Edit';
 import Documents from '../Dashboard/Document/Documents';
 import AddProfileContent from '../Dashboard/Profile/AddProfileContent';
 import Course from '../Dashboard/Course/Course';
 import NoMatch from '../NoMatch';
+
+import Test from '../Dashboard/test';
 
 class Main extends Component {
   render() {
@@ -35,8 +37,8 @@ class Main extends Component {
         />
         <PrivateRoute
           exact
-          path="/projectex"
-          component={Projectex}
+          path="/project/:id"
+          component={Project_Edit}
           authed={this.props.user}
         />
         <PrivateRoute
