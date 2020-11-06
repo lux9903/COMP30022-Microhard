2bf4dea9f15d2071853f1f393e3ddf74a3f43f08
Riley Busch <rbusch@student.unimelb.edu.au>
2020-11-06 11:46:06 +1100

    loading symbol consistant

--- client/src/components/Dashboard/Experience.jsx
+++ client/src/components/Dashboard/Experience.jsx
@@ -39,7 +39,6 @@ const useStyles2 = (theme) => ({
   progress: {
     marginTop: theme.spacing(2),
     marginBottom:theme.spacing(2),
-    color: "white",
   },
 });
 
@@ -388,7 +387,7 @@ class Experience extends Component {
     } else if (isFetching) {
       //show the circular progress bar if database is still process
       content = (
-        <Grid container justify="center" alignItems="center" className={classes.root}>
+        <Grid container justify="center" alignItems="center">
           <CircularProgress color="primary" className={classes.progress}/>
         </Grid>
       );
